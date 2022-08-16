ed8cc225fd6e1643ad78d48697463af99ff0e04c
tyren1995 <tyrenrhinehart@yahoo.com>
2022-08-15 20:37:57 -0400

    create basic app, edit app.js to be more fitting

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>A long time ago - in a galaxy far, far away...</h1>
+    </div>
+  );
+}
+
+export default App;
