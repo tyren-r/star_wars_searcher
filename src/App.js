ba89ebcf0291e6bbcadc8bbaa555993ef98c9573
tyren1995 <tyrenrhinehart@yahoo.com>
2022-08-15 23:35:57 -0400

    a very basic static results table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import SearchBar from './components/search-bar/ui/SearchBar';
+import ResultTable from './components/result-table/ui/ResultTable';
 
 function App() {
   return (
     <div className="App">
       <h1>A long time ago - in a galaxy far, far away...</h1>
       <SearchBar/>
+      <ResultTable/>
     </div>
   );
 }
---
+++ src/components/result-table/ui/ResultTable.js
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function ResultTable() {
+  return (
+    <TableContainer sx={{ minWidth: 350, maxWidth:500}} component={Paper}>
+      <Table  aria-label="simple table">
+        <TableBody>
+
+            <TableRow
+              key={1}
+            >
+              <TableCell component="th" scope="row">Name</TableCell>
+              <TableCell align="right">Luke Skywalker</TableCell>
+            </TableRow>
+
+            <TableRow
+              key={2}
+            >
+              <TableCell component="th" scope="row">Height</TableCell>
+              <TableCell align="right">About 2.5 ewoks</TableCell>
+            </TableRow>
+
+            <TableRow
+              key={3}
+            >
+              <TableCell component="th" scope="row">Mass</TableCell>
+              <TableCell align="right">Light enough to jump really high</TableCell>
+            </TableRow>
+
+            <TableRow
+              key={4}
+            >
+              <TableCell component="th" scope="row">Hair Color</TableCell>
+              <TableCell align="right">youngling blonde</TableCell>
+            </TableRow>
+
+            <TableRow
+              key={5}
+            >
+              <TableCell component="th" scope="row">skin color</TableCell>
+              <TableCell align="right">whiter than Hoth</TableCell>
+            </TableRow>
+
+            <TableRow
+              key={6}
+            >
+              <TableCell component="th" scope="row">Birth Year</TableCell>
+              <TableCell align="right">A long, long time ago</TableCell>
+            </TableRow>
+
+            <TableRow
+              key={7}
+            >
+              <TableCell component="th" scope="row">Films</TableCell>
+              <TableCell align="right">Film 1, Film 2, Film3, Film 4</TableCell>
+            </TableRow>
+
+            <TableRow
+              key={8}
+            >
+              <TableCell component="th" scope="row">Species</TableCell>
+              <TableCell align="right">Human</TableCell>
+            </TableRow>
+
+
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default ResultTable;
