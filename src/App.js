d03e30922dd0e950558f3beda2e1e8b2bcfe0a45
tyren-r <tyrenrhinehart@yahoo.com>
2024-12-29 20:19:34 -0700

    add stars background

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import SearchContainer from "./components/search-container/ui/SearchContainer";
 function App() {
 
   return (
+    <>
+    <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
     <SearchContainer/>
+    </>
   );
 }
 
